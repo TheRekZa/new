79c78d3ee130ee36d83f30f0a306dffccca5a82c
TheRekZa <txzazalnw@gmail.com>
2025-07-24 09:26:29 +0700

    Add files via upload

---
+++ SQLQuery255555.sql
@@ -0,0 +1,3 @@
+select * from Employees
+select EmployeeID , FirstName , LastName , City = 'London' from employees
+select * from Products where CategoryID in (1,2,4,6) where UnitPrice between 50 and 100 
